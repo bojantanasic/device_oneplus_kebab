3fed421737a594f33ebe4a9aa3e657975c35594e
bojantanasic <bojan.tanasic@gmail.com>
2023-06-02 17:00:05 +0200

    kebab: Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_kebab.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_kebab-user \
+    aosp_kebab-userdebug \
+    aosp_kebab-eng
---
+++ aosp_kebab.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from kebab device
+$(call inherit-product, device/oneplus/kebab/device.mk)
+
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_HAS_UDFPS := true	
+
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Next Gen Assistant
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+PRODUCT_NAME := aosp_kebab
+PRODUCT_DEVICE := kebab
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := KB2005
+
+PRODUCT_SYSTEM_NAME := OnePlus8T
+PRODUCT_SYSTEM_DEVICE := OnePlus8T
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus8T-user 13 RKQ1.211119.001 R.ed8c2c_1_2 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := OnePlus/OnePlus8T/OnePlus8T:13/RKQ1.211119.001/R.ed8c2c_1_2:user/release-keys
